d89ca161f8ea94c1e516d527cb01e2d6203c5980
Tomasz Rochala <wartego@wp.pl>
2023-12-22 12:51:01 +0100

    first commit

---
+++ src/main/java/pl/wartego/restapi_springboot_training/RestApiSpringBootTrainingApplication.java
@@ -0,0 +1,13 @@
+package pl.wartego.restapi_springboot_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSpringBootTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiSpringBootTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wartego/restapi_springboot_training/RestApiSpringBootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wartego.restapi_springboot_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringBootTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
